801bee7bfcabdc011f3e6351194d171e913107c7
kat <309544@niuitmo.ru>
2021-09-20 22:11:01 +0300

    title added

--- main.php
+++ main.php
@@ -11,17 +11,24 @@ $y = !empty($_GET['y']) ? $_GET['x'] : 'Координата y не распоз
 $r = !empty($_GET['r']) ? $_GET['y'] : 'Значение R не распознано';
 
 function check($x, $y, $r) {
-    if($x<=1.5 && $r>=0 && $r<=1.5 && $y>=-3 && $y<=0) || ($r>=0 && $r<=1.5 && $x>=0 && $x<=3 && $y>=0 && $y<=1,5) || ($r>=0 && $r<=3 &7 $x>=0 && $x<=3 && $y>=-3 && $y<=0) {
-        return TRUE;
-    }
+    if($x < 1.5 && $r>=0 && $r<=1.5 && $y>=-3 && $y<=0) {
+        $res = true; 
+
+} else if($r>=0 && $r<=1.5 && $x>=0 && $x<=3 && $y>=0 && $y<=1.5) {
+    $res = true; 
+    
+} else if($r>=0 && $r<=3 && $x>=0 && $x<=3 && $y>=-3 && $y<=0) {
+    $res = true; 
+}
     else {
-        return FALSE;
+        $res = false;
     }
+
 }
 
 $res = check($x, $y, $r);
 
-$result = array($x, $y, $r, $currentTime, $time)
+$result = array($x, $y, $r, $res, $currentTime, $time);
 
 $finish = microtime(true);
 $executionTime = $finish-$start;
@@ -31,44 +38,34 @@ array_push($_SESSION["sessionTable"],  "<tr>
 <td>$x</td>
 <td>$y</td>
 <td>$r</td>
-<td>$coordsStatus</td>
+<td>$res</td>
 <td>$currentTime</td>
 <td>$executionTime</td>
 
-echo "<table>
+echo '<table>
 <tr>
-    <th>x</th>
-    <th>y</th>
-    <th>r</th>
-    <th>Точка входит в ОДЗ</th>
-    <th>Текущее время</th>
-    <th>Время работы скрипта</th>
-</tr>";
+    <td>x</td>
+    <td>y</td>
+    <td>r</td>
+    <td>Точка входит в ОДЗ</td>
+    <td>Текущее время</td>
+    <td>Время работы скрипта</td>
+</tr>';
 
 foreach ($_GET['x', 'y', 'r'] as $x, $y, $r) {
 $_SESSION['x', 'y', 'r'] = $_GET['x', 'y', 'r'];
             $rows = get_info($x,$y, $r);
-            if (count($rows)){
+            if (count($rows)) {
                 foreach ($rows as $row) {       
                     $_SESSION['row'] = $rows;
                     echo "<tr>" .
-                        "<td>" . $row["x"] . "</td>" .
-                        "<td>" . $row["y"] . "</td>" .
-                        "<td>" . $row["r"] . "</td>" .
-                        "<td>" . $row["status"] . "</td>" .
-                        "<td>" . $row["curTime"] . "</td>" .
-                        "<td>" . $row["exTime"] . "</td>" .
+                        "<td>" . $row['x'] . "</td>" .
+                        "<td>" . $row['y'] . "</td>" .
+                        "<td>" . $row['r'] . "</td>" .
+                        "<td>" . $row['status'] . "</td>" .
+                        "<td>" . $row['curTime'] . "</td>" .
+                        "<td>" . $row['exTime'] . "</td>" .
                         "</tr>";
 echo "</table>";
-
-
-
-
-
-
-
-
-
-
 ?>
  
